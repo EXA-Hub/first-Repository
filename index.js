11c419452ead43478b0967bf323414a49ca89532
EXA-Hub <89308682+EXA-Hub@users.noreply.github.com>
2021-09-06 01:13:19 +0200

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,25 @@ const client = new Discord.Client();
 const config = require('./config.json');
 const prefix = config.prefix;
 
+client.on('ready', () => {
+    console.log(`${client.user.username} is ready`);
+});
 
+client.on('message', message => {
+    if (!message.content.startsWith(prefix)) return;
+    if (!message.guild) return;
+    if (message.author.bet) return;
+
+    const args = message.content.slice(prefix.length).split(/ +/g);
+    switch (args[0]) {
+        case 'ping':
+            message.channel.send(Math.round(client.ws.ping));
+            message.channel.send(Date.now() - message.createdTimestamp);
+            break;
+        case 'time':
+            message.channel.send(Date.now());
+            break;
+    }
+});
 
 client.login(config.token);
